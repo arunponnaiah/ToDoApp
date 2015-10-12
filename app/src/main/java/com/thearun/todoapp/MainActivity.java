874b845702311779266b90b00214b072d4283ce0
Arun Ponnaiah <arun.ponnaiah@gmail.com>
2015-10-12 16:42:07 -0700

    updated README

--- app/src/main/java/com/thearun/todoapp/MainActivity.java
+++ app/src/main/java/com/thearun/todoapp/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
                 items.remove(pos);
                 itemsAdapter.notifyDataSetChanged();
                 writeItems();
-                return true;
             }
 
         });
