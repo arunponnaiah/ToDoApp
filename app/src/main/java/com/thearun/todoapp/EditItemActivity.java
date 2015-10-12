064a97025f518167a485dcd0ae1e74efaf1d8314
Arun Ponnaiah <arun.ponnaiah@gmail.com>
2015-10-11 23:01:50 -0700

    remove title of edit activity

--- app/src/main/java/com/thearun/todoapp/EditItemActivity.java
+++ app/src/main/java/com/thearun/todoapp/EditItemActivity.java
@@ -3,7 +3,6 @@ package com.thearun.todoapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,8 +12,6 @@ public class EditItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_item);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         String editItem = getIntent().getStringExtra("editItem");
 
